1c9a8a0c6681bfad6fee10716ee5e411044b79a7
chaming-yu <chamingyu1213@gmail.com>
2023-06-06 02:22:45 +0900

    gugudanMap

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <GuguUsingForLoop /> */}
-      <GuguUsingMap />
+      <GuguUsingMap 사용이름은내마음대로="전달할데이터" 사용2="데이터2" />
+      <GuguUsingMap 사용이름은내마음대로="복사한놈1" 사용2="복사한놈2" />
     </div>
   );
 }
--- components/guguUsingMap.jsx
+++ components/guguUsingMap.jsx
@@ -1,3 +1,25 @@
-export default function GuguUsingMap() {
-  return <div> map </div>;
+export default function GuguUsingMap(props) {
+  const { 사용이름은내마음대로, 사용2 } = props;
+
+  const num1 = [2, 3, 4, 5, 6, 7, 8, 9];
+  const num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const number = num1.map((i) => {
+    return (
+      <div>
+        {i + "단"}
+        {num2.map((j) => {
+          return <div>{i + "X" + j + "=" + i * j}</div>;
+        })}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div>{사용이름은내마음대로}</div>
+      <div>{사용2}</div>
+      <div>{number}</div>
+    </div>
+  );
 }
